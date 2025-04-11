31254b55e9d71e01d0b099ecf633b420120a084b
JsTsDev Pinky <RosemaryR784@outlook.com>
2025-04-11 13:36:36 -0400

    deploy big burger man

---
+++ js/main.js
@@ -0,0 +1,156 @@
+$( document ).ready( function() {
+
+  if ( $( '.slick-item' ).length ) {
+      $('.slick-item').slick({
+          dots: false,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+          adaptiveHeight: true,
+          arrows:true
+      });
+  }
+
+  if ( $( '.product-slick' ).length ) {
+    $('.product-slick').slick({
+      dots: false,
+      arrows: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+ }
+
+ if ( $( '.posts-slick' ).length ) {
+  $('.posts-slick').slick({
+    dots: false,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+ }
+
+ if ( $( '.logos' ).length ) {
+  $('.logos').slick({
+    dots: false,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+ } 
+
+ if ( $( '.testimonial-slick' ).length ) {
+  $(".testimonial-slick").slick({
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    lazyLoad: "ondemand",
+    responsive: [
+      {
+        breakpoint: 1250,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+ }
+
+ if ( $( '.about-slick' ).length ) {
+  $(".about-slick").slick({
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    lazyLoad: "ondemand",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+ }
+
+});
